7ee16b4e04261cbfdb944926b7b8093b34d471d9
Mehul Goel <mehulgoel@10-21-114-221.dynapool.wireless.nyu.edu>
2026-02-25 16:17:32 -0500

    chore(agents): export search_agent from agents package

--- backend/agents/__init__.py
+++ backend/agents/__init__.py
@@ -1,4 +1,5 @@
 from .planner import planner_agent
+from .search import search_agent
 from .state import ResearchState
 
-__all__ = ["planner_agent", "ResearchState"]
+__all__ = ["planner_agent", "search_agent", "ResearchState"]
