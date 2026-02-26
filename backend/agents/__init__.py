1a604c2f200ebbbe17ca7df38d1c39330c7a0d66
Mehul Goel <mehulgoel@10-17-59-50.dynapool.wireless.nyu.edu>
2026-02-25 19:29:19 -0500

    chore(agents): export writer_agent from agents package

--- backend/agents/__init__.py
+++ backend/agents/__init__.py
@@ -1,6 +1,7 @@
 from .planner import planner_agent
 from .search import search_agent
 from .critic import critic_agent
+from .writer import writer_agent
 from .state import ResearchState
 
-__all__ = ["planner_agent", "search_agent", "critic_agent", "ResearchState"]
+__all__ = ["planner_agent", "search_agent", "critic_agent", "writer_agent", "ResearchState"]
