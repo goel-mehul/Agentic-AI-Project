da2603c4e1f696f528da3648758a610dcdd6693e
Mehul Goel <mehulgoel@10-17-59-50.dynapool.wireless.nyu.edu>
2026-02-25 19:45:35 -0500

    chore(agents): export fact_checker_agent from agents package

--- backend/agents/__init__.py
+++ backend/agents/__init__.py
@@ -2,6 +2,7 @@ from .planner import planner_agent
 from .search import search_agent
 from .critic import critic_agent
 from .writer import writer_agent
+from .fact_checker import fact_checker_agent
 from .state import ResearchState
 
-__all__ = ["planner_agent", "search_agent", "critic_agent", "writer_agent", "ResearchState"]
+__all__ = ["planner_agent", "search_agent", "critic_agent", "writer_agent", "fact_checker_agent", "ResearchState"]
